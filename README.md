55de667061ed585224610dd8249b596e63ee7315
Isaac Sears <isaac.j.sears@gmail.com>
2021-02-11 11:18:59 -0600

    Make gpu optimizations optional and document

--- README.md
+++ README.md
@@ -21,6 +21,19 @@ img_clahe = tf_clahe.clahe(img)
 ```
 ![sidebyside example](./example.png)
 
+## Optimizing for GPU with XLA
+
+A considerable performance improvement can be achieved by using the `gpu_optimized` flag 
+in combination with XLA compilation. For example:
+
+```python
+import tf_clahe
+import tensorflow as tf
+
+@tf.function(experimental_compile=True)  # Enable XLA
+def fast_clahe(img):
+    return tf_clahe.clahe(img, gpu_optimized=True)
+```
 ## References
 
 - [S. M. Pizer, E. P. Amburn, J. D. Austin, et al.: Adaptive Histogram Equalization and Its Variations. Computer Vision, Graphics, and Image Processing 39 (1987) 355-368.](https://www.cs.unc.edu/Research/Image/MIDAG/pubs/papers/Adaptive%20Histogram%20Equalization%20and%20Its%20Variations.pdf)
--- tests/clahe_test.py
+++ tests/clahe_test.py
@@ -19,11 +19,12 @@ _DTYPES = {
 _SHAPES = {(5, 5), (5, 5, 1), (5, 5, 3), (4, 5, 5), (4, 5, 5, 1), (4, 5, 5, 3)}
 
 
-def test_clahe_with_equalize():
+@pytest.mark.parametrize("gpu_optimized", [True, False])
+def test_clahe_with_equalize(gpu_optimized):
     np.random.seed(0)
     image = np.random.randint(low=0, high=255, size=(5, 100, 100, 3), dtype=np.uint8)
     # CLAHE w/grid size 1x1 and no clip limit should in theory just be global equalization
-    clahed = tf_clahe.clahe(image, clip_limit=0, tile_grid_size=(1, 1))
+    clahed = tf_clahe.clahe(image, clip_limit=0, tile_grid_size=(1, 1), gpu_optimized=gpu_optimized)
     equalized = tfa.image.equalize(image)
 
     # Atol 1 to account for rounding differences between two methods
@@ -32,10 +33,11 @@ def test_clahe_with_equalize():
 
 @pytest.mark.parametrize("dtype", _DTYPES)
 @pytest.mark.parametrize("shape", _SHAPES)
-def test_clahe_dtype_shape(dtype, shape):
+@pytest.mark.parametrize("gpu_optimized", [True, False])
+def test_clahe_dtype_shape(dtype, shape, gpu_optimized):
     image = np.ones(shape=shape, dtype=dtype)
     clahed = tf_clahe.clahe(
-        tf.constant(image), clip_limit=2.0, tile_grid_size=(2, 2)
+        tf.constant(image), clip_limit=2.0, tile_grid_size=(2, 2), gpu_optimized=gpu_optimized
     ).numpy()
     assert clahed.dtype == image.dtype
     assert clahed.shape == image.shape
